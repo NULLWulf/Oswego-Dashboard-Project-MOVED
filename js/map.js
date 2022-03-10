d213c0b1f545aed7305800b0f664024f819ac006
NULLWulf <89611095+NULLWulf@users.noreply.github.com>
2022-03-10 12:10:45 -0500

    added css file and minor changes

--- js/map.js
+++ js/map.js
@@ -1,46 +1,53 @@
 mapboxgl.accessToken =
-  "pk.eyJ1IjoibmR3b2xmMTk5MSIsImEiOiJjbDA4aGppczcwM2kzM2pxdHZydmdsYm5yIn0.ZPuI0T1FxHGAJu_wklsSXg";
+  "pk.eyJ1IjoibmR3b2xmMTk5MSIsImEiOiJjbDA4aGppczcwM2kzM2pxdHZydmdsYm5yIn0.ZPuI0T1FxHGAJu_wklsSXg"; // public token, not able to make changes to map itself with it
+// only access style layer etc.
 
 const map = new mapboxgl.Map({
+  // creates Mapbox object
   container: "map", // container ID
   style: "mapbox://styles/ndwolf1991/cl0ikst93000j15p45jdekxmf", // style URL
   center: [-76.543134, 43.453054], // starting position [lng, lat]
-  zoom: 16.02, // starting zoom
-  bearing: -37.25,
-  pitch: 0,
+  zoom: 16.02, // initial zoom start
+  bearing: -37.25, // slightly off north to show majority of campus
+  pitch: 0, // directly overhead
 });
 
+function bondFeatures(bound, map, event) {
+  // function to get data features underneath point when an event is passed through
+  const bbox = [
+    // based off of pixel width to determine bounds for
+    [event.point.x - bound, event.point.y - bound],
+    [event.point.x + bound, event.point.y + bound],
+  ];
+
+  return map.queryRenderedFeatures(bbox, { layers: ["buildings"] }); // returns Objecct that corresponds with data values under point
+  // bounds are passed in so you can tweak the clickable radius of elements (in this case using as a stock incon for a target, unsure
+  // of methods that allow of interacting with stock map elemtns )
+}
+
 map.on("load", () => {
-  map.addControl(new mapboxgl.NavigationControl());
+  // On initial map load performs these functions
+  map.addControl(new mapboxgl.NavigationControl()); // adds basioc control structure to document, can be further modified
 });
 
 map.on("click", (event) => {
-  const point = event.point;
+  // more click events to come buttrently grabs features under point
+  const features = bondFeatures(3, map, event);
 
-  const features = map.queryRenderedFeatures([point.x, point.y], {
-    layers: ["buildings"],
-  });
-
-  // console.log(features[0].properties);
-  // window.alert(features[0].properties.Name);
-
-  location.href =
-    "https://www.oswego.edu/facilities-services/facilities-services-0";
+  console.log(features[0].properties);
+  window.alert(features[0].properties.Name); // displays DOM pop up of prperty name, will be eventually set to that click through do things like redirects, open panels etc
 });
 
-map.on("mousemove", (e) => {
-  const point = e.point;
-  const features = map.queryRenderedFeatures([point.x, point.y], {
-    layers: ["buildings"],
-  });
+map.on("mousemove", (event) => {
+  // tracks geoloc respective of map, coordinations repsective of where map is framed and building name if applicable.
+  // currently does not reset upon moving off of a building
+  const features = bondFeatures(3, map, event);
 
-  document.getElementById("info").innerHTML =
-    // `e.point` is the x, y coordinates of the `mousemove` event
-    // relative to the top-left corner of the map.
-    JSON.stringify(e.point) +
-    "<br />" +
-    // `e.lngLat` is the longitude, latitude geographical position of the event.
-    JSON.stringify(e.lngLat.wrap()) +
-    "<br />" +
-    JSON.stringify(features[0].properties.Name);
+  document.getElementById("building").innerHTML = JSON.stringify(
+    features[0].properties.Name
+  );
+  document.getElementById("geoloc").innerHTML = JSON.stringify(event.point);
+  document.getElementById("coords").innerHTML = JSON.stringify(
+    event.lngLat.wrap()
+  );
 });
