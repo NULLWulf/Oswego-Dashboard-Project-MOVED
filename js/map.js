110ae6df405a83a6c4623794c04dbf3ef171eb2b
NULLWulf <89611095+NULLWulf@users.noreply.github.com>
2022-03-10 11:00:20 -0500

    more restructuring

--- js/map.js
+++ js/map.js
@@ -1,4 +1,5 @@
 mapboxgl.accessToken = "pk.eyJ1IjoibmR3b2xmMTk5MSIsImEiOiJjbDA4aGppczcwM2kzM2pxdHZydmdsYm5yIn0.ZPuI0T1FxHGAJu_wklsSXg";
+
 const map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/ndwolf1991/cl0ikst93000j15p45jdekxmf", // style URL
@@ -9,39 +10,33 @@ const map = new mapboxgl.Map({
 });
 
 map.on("load", () => {
-  map.on("click", (event) => {
-    const point = event.point;
-
-    const features = map.queryRenderedFeatures([point.x, point.y], {
-      layers: ["buildings"],
-    });
-
-    console.log(features[0].properties);
-    window.alert(features[0].properties.Name);
-
-    // Code from the next step will go here.
-  });
-
   map.addControl(new mapboxgl.NavigationControl());
-
-  map.on("mousemove", (e) => {
-    const point = e.point;
-    const features = map.queryRenderedFeatures([point.x, point.y], {
-      layers: ["buildings"],
-    });
-
-    document.getElementById("info").innerHTML =
-      // `e.point` is the x, y coordinates of the `mousemove` event
-      // relative to the top-left corner of the map.
-      JSON.stringify(e.point) +
-      "<br />" +
-      // `e.lngLat` is the longitude, latitude geographical position of the event.
-      JSON.stringify(e.lngLat.wrap()) +
-      "<br />" +
-      JSON.stringify(features[0].properties.Name);
-  });
-
-  map.on("mousemove", (e) => {});
 });
 
+map.on("click", (event) => {
+  const point = event.point;
 
+  const features = map.queryRenderedFeatures([point.x, point.y], {
+    layers: ["buildings"],
+  });
+
+  console.log(features[0].properties);
+  window.alert(features[0].properties.Name);
+});
+
+map.on("mousemove", (e) => {
+  const point = e.point;
+  const features = map.queryRenderedFeatures([point.x, point.y], {
+    layers: ["buildings"],
+  });
+
+  document.getElementById("info").innerHTML =
+    // `e.point` is the x, y coordinates of the `mousemove` event
+    // relative to the top-left corner of the map.
+    JSON.stringify(e.point) +
+    "<br />" +
+    // `e.lngLat` is the longitude, latitude geographical position of the event.
+    JSON.stringify(e.lngLat.wrap()) +
+    "<br />" +
+    JSON.stringify(features[0].properties.Name);
+});
