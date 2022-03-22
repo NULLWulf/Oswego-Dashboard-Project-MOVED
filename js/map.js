576d0115dbde5535503ba6de0c4e14b65742ea26
NULLWulf <89611095+NULLWulf@users.noreply.github.com>
2022-03-22 10:13:41 -0400

    Started building pop up feature

--- js/map.js
+++ js/map.js
@@ -34,39 +34,69 @@ map.on("load", () => {
   console.log("Map Loaded");
 });
 
-map.on("click", (event) => {
-  // more click events to come buttrently grabs features under point
+// map.on("click", (event) => {
+//   // more click events to come buttrently grabs features under point
+//   const features = bondFeatures(_bounds, map, event);
+
+//   if (features.length) {
+//     // if features exists executes the following (implemented to prevent undefined errors otherwise)
+//     console.log(features[0].properties);
+//     window.open(
+//       `https://aim.sucf.suny.edu/fmax/screen/MASTER_ASSET_VIEW?assetTag=${features[0].properties.assetID}`
+//     );
+//   }
+// });
+
+// map.on("mousemove", (event) => {
+//   // tracks geoloc respective of map, coordinations repsective of where map is framed and building name if applicable.
+//   // currently does not reset upon moving off of a building
+//   const features = bondFeatures(_bounds, map, event);
+
+//   if (features.length) {
+//     document.getElementById("building").innerHTML = JSON.stringify(
+//       features[0].properties.name
+//     );
+//     document.getElementById("coords").innerHTML = JSON.stringify(event.point);
+//     document.getElementById("geoloc").innerHTML = JSON.stringify(
+//       event.lngLat.wrap()
+//     );
+//   } else {
+//     document.getElementById("building").innerHTML = "N/a";
+//     document.getElementById("coords").innerHTML = JSON.stringify(event.point);
+//     document.getElementById("geoloc").innerHTML = JSON.stringify(
+//       event.lngLat.wrap()
+//     );
+//   }
+// });
+
+map.on("click", "buildings", (event) => {
+  // Copy coordinates array.
+  const coordinates = event.features[0].geometry.coordinates.slice();
   const features = bondFeatures(_bounds, map, event);
 
-  if (features.length) {
-    // if features exists executes the following (implemented to prevent undefined errors otherwise)
-    console.log(features[0].properties);
-    window.open(
-      `https://aim.sucf.suny.edu/fmax/screen/MASTER_ASSET_VIEW?assetTag=${features[0].properties.assetID}`
-    );
+  // Ensure that if the map is zoomed out such that multiple
+  // copies of the feature are visible, the popup appears
+  // over the copy being pointed to.
+  while (Math.abs(event.lngLat.lng - coordinates[0]) > 180) {
+    coordinates[0] += event.lngLat.lng > coordinates[0] ? 360 : -360;
   }
+
+  new mapboxgl.Popup()
+    .setLngLat(coordinates)
+    .setHTML(
+      "<h3>" +
+        features[0].properties.name +
+        "</h3>" +
+        '<a href="https://aim.sucf.suny.edu/fmax/screen/MASTER_ASSET_VIEW?assetTag=${features[0].properties.assetID} target="_blank">AIM Asset View</a>' // _blank not working?
+    )
+    .addTo(map);
 });
 
-map.on("mousemove", (event) => {
-  // tracks geoloc respective of map, coordinations repsective of where map is framed and building name if applicable.
-  // currently does not reset upon moving off of a building
-  const features = bondFeatures(_bounds, map, event);
+map.on("mouseenter", "buildings", () => {
+  map.getCanvas().style.cursor = "pointer";
+});
 
-  map.getCanvas().style.cursor = features.length ? "pointer" : "";
-
-  if (features.length) {
-    document.getElementById("building").innerHTML = JSON.stringify(
-      features[0].properties.name
-    );
-    document.getElementById("coords").innerHTML = JSON.stringify(event.point);
-    document.getElementById("geoloc").innerHTML = JSON.stringify(
-      event.lngLat.wrap()
-    );
-  } else {
-    document.getElementById("building").innerHTML = "N/a";
-    document.getElementById("coords").innerHTML = JSON.stringify(event.point);
-    document.getElementById("geoloc").innerHTML = JSON.stringify(
-      event.lngLat.wrap()
-    );
-  }
+// Change it back to a pointer when it leaves.
+map.on("mouseleave", "buildings", () => {
+  map.getCanvas().style.cursor = "";
 });
