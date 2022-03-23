50b4ecaa03ef602ee0dcbffe0a09af9f68ab01ec
NULLWulf <89611095+NULLWulf@users.noreply.github.com>
2022-03-23 08:55:40 -0400

    added additional pictures, also added data issues file

--- js/map.js
+++ js/map.js
@@ -68,15 +68,16 @@ map.on("click", "buildings", (event) => {
     coordinates[0] += event.lngLat.lng > coordinates[0] ? 360 : -360;
   }
 
-  const popupHtml = `<div id="popup" class="popup">
-  <h2>${features[0].properties.name}</h2>
-  <img src="images/building-images/${features[0].properties.buildingNo}.jpg" alt="Image of ${features[0].properties.name}"></img>
-  </br>
-  <strong>Building No: </strong>${features[0].properties.buildingNo}
-  </br>
-  <strong>Ft<sup>2</sup>: </strong>${features[0].properties.squareFt}
-  </br>
-  <a href="https://aim.sucf.suny.edu/fmax/screen/MASTER_ASSET_VIEW?assetTag=${features[0].properties.assetID}" target="_blank">AIM Asset View</a>
+  const popupHtml = `
+  <div id="popup" class="popup">
+    <h2>${features[0].properties.name}</h2>
+    <img src="images/building-images/${features[0].properties.buildingNo}.jpg" alt="Image of ${features[0].properties.name}"></img>
+    </br>
+    <strong>Building No: </strong>${features[0].properties.buildingNo}
+    </br>
+    <strong>Ft<sup>2</sup>: </strong>${features[0].properties.squareFt}
+    </br>
+    <a href="https://aim.sucf.suny.edu/fmax/screen/MASTER_ASSET_VIEW?assetTag=${features[0].properties.assetID}" target="_blank">AIM Asset View</a>
   </div> `;
 
   new mapboxgl.Popup()
