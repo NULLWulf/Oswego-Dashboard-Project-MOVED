c06ad75c2f50ae48aee67abc9c50596467444006
NULLWulf <89611095+NULLWulf@users.noreply.github.com>
2022-04-08 13:33:01 -0400

    merging sidebar changes into main

--- js/jquery.js
+++ js/jquery.js
@@ -1,9 +1,6 @@
-$(document).keydown(function (e) {
-  var key = e.key;
-  if (key == 116) {
-    // if the user pressed 't':
-    if (!$("body").hasClass(".info-panel"))
-      $("body").addClass(".info-panel").css("opacity", "0");
-    else $("body").removeClass(".info-panel").css("opacity", "1");
+$(document).ready(function () {
+  var width = $(window).width();
+  if (width < 767) {
+    alert("Your screen is too small");
   }
 });
